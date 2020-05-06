f1686b59bdb9236a06c768b981386801189b9955
Nico Sinisterra <nsinisterra@bondacom.com>
2020-05-06 16:37:10 -0300

    feat: add locales folder

---
+++ locales/es.lua
@@ -0,0 +1,134 @@
+Locales['es'] = {
+  ['box_title'] = 'animaciones',
+  
+  ['festives'] = 'movimientos',
+  ['smoke'] = 'fumar',
+  ['musician'] = 'tocar música',
+  ['dj'] = 'dj',
+  ['coffee'] = 'tomar café',
+  ['beer'] = 'cerveza',
+  ['air_guitar'] = 'guitarra',
+  ['air_shagging'] = 'fuck it',
+  ['rocknroll'] = 'rock n roll',
+  ['drunk_standing'] = 'emborracharse',
+  ['vomiting'] = 'vomitar',
+
+  ['greetings'] = 'saludos',
+  ['hello'] = 'hola',
+  ['wave'] = 'saludo con la mano',
+  ['handshake'] = 'apretón de mano',
+  ['hugging'] = 'abrazo',
+  ['salute'] = 'saludo militar',
+
+  ['work'] = 'trabajo',
+  ['suspect_surrender'] = 'sospechoso: Entregarse',
+  ['finishing'] = 'morir',
+  ['police_investigate'] = 'policía: Investigar',
+  ['police_use_radio'] = 'policía: Usar la radio',
+  ['police_traffic'] = 'policía: Dirigir el tráfico',
+  ['police_binoculars'] = 'policía: Binoculares',
+  ['agriculture_planting'] = 'agricultór: PLantar',
+  ['machanic_fix_motor'] = 'mecánico: Arreglar el motor',
+  ['machanic_kneel'] = 'mecánico: arrodillarse',
+  ['taxi_talk_to_customer'] = 'taxista: Hablar con el cliente',
+  ['taxi_give_bill'] = 'taxista: Entregar boleta',
+  ['grocer_give'] = 'dependiente: Entregar mercadería',
+  ['barman_serve_shot'] = 'barman: Servir un shot',
+  ['journalist_take_photos'] = 'periodista: Tomar fotos',
+  ['all_jobs_clipboard'] = 'todos: Anotar',
+  ['all_jobs_hammering'] = 'todos: Martillar',
+  ['bum_holding_sign'] = 'Vagabund: Limosna',
+  ['bum_human_statue'] = 'Vagabundo: Estatua viviente',  
+
+  ['humors'] = 'saludos',
+  ['cheering'] = 'aplausos',
+  ['super'] = 'gracias',
+  ['point'] = 'apuntar',
+  ['come_here'] = 'ven aquí',
+  ['bring_it_on'] = '¡Dale!',
+  ['me'] = 'me',
+  ['i_knew_it'] = '¡Lo sabía!',
+  ['exhausted'] = 'Cansado',
+  ['im_in_the_shit'] = 'Estoy en la mierda',
+  ['facepalm'] = 'Facepalm',
+  ['calm_down'] = 'Calmate',
+  ['what_did_i_do'] = 'Qué he hecho?',
+  ['fear'] = 'Miedo',
+  ['fight'] = 'Quieres pelear ?',
+  ['its_not_possible'] = 'No puede ser!',
+  ['embrace'] = 'abrazo',
+  ['finger_of_honor'] = 'Fuck you!',
+  ['you_wanker'] = '¡Idiota!',
+  ['bullet_in_the_head'] = 'Balazo en la cabeza',
+
+  ['sports'] = 'Deportista',
+  ['flex_muscles'] = 'mostrar músculos',
+  ['lift_weights'] = 'lift weights',
+  ['do_push_ups'] = 'do push ups',
+  ['do_sit_ups'] = 'do sit ups',
+  ['do_yoga'] = 'do yoga',
+
+  ['misc'] = 'divers',
+  ['drink_coffee'] = 'Tomar café',
+  ['sit'] = 'Sentarse',
+  ['lean_against_wall'] = 'Apoyarse en la pared',
+  ['sunbathe_back'] = 'acostarse boca arriba',
+  ['sunbathe_front'] = 'acostarse boca abajo',
+  ['clean'] = 'Limpiarse',
+  ['bbq'] = 'BBQ',
+  ['search'] = 'buscar',
+  ['take_a_selfie'] = 'sacarse una selfie',
+  ['listen_to_wall_door'] = 'escuchar tras la puerta',
+
+  ['attitudem'] = 'Estilos de caminar'
+  ['normal_m'] = 'masculino normal',
+  ['normal_f'] = 'femenino normal',
+  ['depressed_male'] = 'masculino deprimido',
+  ['depressed_female'] = 'femenino deprimido',
+  ['business'] = 'empresario',
+  ['determined'] = 'determinado',
+  ['casual'] = 'casual',
+  ['ate_too_much'] = 'comí demaciado',
+  ['hipster'] = 'hipster',
+  ['injured'] = 'lastimado',
+  ['in_a_hurry'] = 'en apuros',
+  ['hobo'] = 'obrero temporal',
+  ['sad'] = 'triste',
+  ['muscle'] = 'musculoso',
+  ['shocked'] = 'impactado',
+  ['being_shady'] = 'sombrio',
+  ['buzzed'] = 'cansado',
+  ['hurry'] = 'apurado',
+  ['proud'] = 'orgulloso',
+  ['short_race'] = 'sprint corto',
+  ['man_eater'] = 'hombre comedor',
+  ['sassy'] = 'descadao',
+  ['arrogant'] = 'arrogante',
+
+  ['porn'] = '+18',
+  ['man_receiving_in_car'] = 'Hombre recibiendo pete en el auto',
+  ['woman_giving_in_car'] = 'Mujer haciendo pete en el auto',
+  ['man_on_bottom_in_car'] = 'Hombre abajo en el auto',
+  ['woman_on_top_in_car'] = 'Mujer arriba en el auto',
+  ['scratch_nuts'] = 'Rascarse las bolas',
+  ['hooker_1'] = 'Puta 1',
+  ['hooker_2'] = 'Puta 2',
+  ['hooker_3'] = 'Puta 3',
+  ['strip_tease_1'] = 'Strip Tease 1',
+  ['strip_tease_2'] = 'Strip Tease 2',
+  ['stip_tease_on_knees'] = 'Stip Tease de rodillas'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
